918dc5ba56b50dfbccfb0596404d8af962af2fe5
KRLEESEUNGJAE <karmaup0711@gmail.com>
2022-03-14 23:12:14 +0900

    Add api document using Swagger

--- a-nest/src/main.ts
+++ a-nest/src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  // API문서(Swagger)
+  const config = new DocumentBuilder()
+    .setTitle('Sleact API')
+    .setDescription('Sleact 개발을 위한 API 문서입니다. Swagger~!')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .addTag('karma addTag')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   console.log(`\n      🚀 listening on port ${port}\n`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
